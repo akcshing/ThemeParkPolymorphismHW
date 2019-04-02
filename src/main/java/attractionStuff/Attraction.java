43377ae13932c77e9abd977557fcc1848ac8c381
Alexkcshing <alexkcshing@gmail.com>
2019-04-02 17:16:46 +0100

    set up abstract classes and visitor class

--- src/main/java/attractionStuff/Attraction.java
+++ src/main/java/attractionStuff/Attraction.java
@@ -7,5 +7,9 @@ public abstract class Attraction {
     public Attraction(String name){
         this.name = name;
     }
-    
+
+    public String getName(){
+        return this.name;
+    }
+
 }
--- src/main/java/stallStuff/Stall.java
+++ src/main/java/stallStuff/Stall.java
@@ -1,4 +1,14 @@
 package stallStuff;
 
 public abstract class Stall {
+
+    private String name;
+    private String ownerName;
+    private int parkingSpot;
+
+    public Stall(String name, String ownerName, int parkingSpot){
+        this.name = name;
+        this.ownerName = ownerName;
+        this.parkingSpot = parkingSpot;
+    }
 }
